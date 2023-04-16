e8b5eeae62e79e5b3adcb4c5ceed463527849699
bobi <andrey.bardashevsky@gmail.com>
2023-04-16 02:11:21 +0200

    - Update minimum supported Intellij version - updated build scripts - updated with latest template - updated gradle - added dynamic classpath builder

--- src/main/kotlin/com/github/bobi/osgiannotationprocessor/settings/OSGIScrProjectSettings.kt
+++ src/main/kotlin/com/github/bobi/osgiannotationprocessor/settings/OSGIScrProjectSettings.kt
@@ -1,9 +1,6 @@
 package com.github.bobi.osgiannotationprocessor.settings
 
-import com.intellij.openapi.components.PersistentStateComponent
-import com.intellij.openapi.components.State
-import com.intellij.openapi.components.Storage
-import com.intellij.openapi.components.service
+import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.annotations.OptionTag
 
@@ -16,6 +13,7 @@ import com.intellij.util.xmlb.annotations.OptionTag
     name = "OsgiScrSettings",
     storages = [(Storage("osgi-scr.xml"))]
 )
+@Service(Service.Level.PROJECT)
 class OSGIScrProjectSettings : PersistentStateComponent<OSGIScrProjectSettings>, OSGIScrConfiguration {
 
     @OptionTag
